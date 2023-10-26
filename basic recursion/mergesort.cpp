57b1163b967b52d651eac736a2372f3ec46ed6bf
Aditya <singhaditya9770@gmail.com>
2023-10-26 13:31:21 +0530

    mergesort

---
+++ basic recursion/mergesort.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+     
+
+void merge(vector<int>arr1, vector<int> arr2){
+    
+}     
+
+void mergesort(vector<int> arr,int l, int u){
+
+
+}
+
+int main(){
+
+    vector<int> arr = {6,4,5,3,1,2};
+    mergesort(arr,0,5);
+    int i=0;
+    while(i<arr.size()){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
